4a0f9b49fe3071815d96727c2462451d363dcf70
Maikel <palango4887@gmail.com>
2023-05-17 12:42:24 +0200

    repositorio de alfonso

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -2,7 +2,11 @@
 public class PrincipalApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Esto e' P.R, dime a quién tú va' a frontearle, yeh yeh\r\n"
+				+ "Así soy yo (¿Ah?)\r\n"
+				+ "Y nunca vo'a bajarle, yeh\r\n"
+				+ "Baja pa'l calentón\r\n"
+				+ "Pa' ver si e' verdad que tú va' a matarme");
 
 	}
 
