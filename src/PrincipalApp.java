1a008a17fd57e01b8510ada4a682088a0b76ce60
Maikel <palango4887@gmail.com>
2023-05-17 12:46:01 +0200

    cambio

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,13 +1,14 @@
-
+import java.util.*;
 public class PrincipalApp {
-
+static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Esto e' P.R, dime a quién tú va' a frontearle, yeh yeh\r\n"
 				+ "Así soy yo (¿Ah?)\r\n"
 				+ "Y nunca vo'a bajarle, yeh\r\n"
 				+ "Baja pa'l calentón\r\n"
 				+ "Pa' ver si e' verdad que tú va' a matarme");
-
+		
+	
 	}
 
 }
